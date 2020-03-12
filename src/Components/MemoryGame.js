367076b71583aebcdfc1884a0ed0937cec314e2d
baphyv <baptiste.hyver@gmail.com>
2020-03-12 15:07:28 +0100

    implemented basic CSS and logic in the MemoryGame component

--- src/Components/MemoryGame.js
+++ src/Components/MemoryGame.js
@@ -1,20 +1,57 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+
+import '../CSS/memoryGame.css'
+
 import { Link } from 'react-router-dom';
+import Bounce from 'react-reveal'
+
+import { frontFace, backFace } from '../DataImages';
 
 const MemoryGame = () => {
+	const handleClick = event => {
+		event.currentTarget.className === 'card'
+			? (event.currentTarget.className = 'card flip')
+			: (event.currentTarget.className = 'card');
+	};
+
+	const cards = [];
+
+	for (let i = 0; i < 12; i++) {
+		for (let j = 0; j < 2; j++) {
+			cards.push(
+				<Fragment key={i}>
+					<div className="card" onClick={handleClick}>
+						<img className="frontCard" src={frontFace[i].img} alt={frontFace[i].name} />
+						<img className="backCard" src={backFace} alt="back face" />
+					</div>
+				</Fragment>
+			);
+		}
+	}
+
 	return (
-		<div>
-			<h1>
-				<u>Memory Game</u>
-			</h1>
-			<div>
-				<h2>Timer: 'time remaining'</h2>
-				<h2>Click: 'number of clicks'</h2>
+		<div className="memoryPage">
+			<Bounce top>
+				<h1 className="title">Memory game</h1>
+			</Bounce>
+			<div className="timeAndClick">
+				<Bounce left>
+					<h2>timer: 'time remaining'</h2>
+				</Bounce>
+				<Bounce right>
+					<h2>click: 'number of clicks'</h2>
+				</Bounce>
 			</div>
-			<div>{/* CARDS */}</div>
-			<Link to="/">
-				<button>Options</button>
-			</Link>
+			<Bounce>
+				<div className="cards">{cards}</div>
+			</Bounce>
+			<Bounce bottom>
+				<div className="optionsButton">
+					<Link to="/">
+						<button>Options</button>
+					</Link>
+				</div>
+			</Bounce>
 		</div>
 	);
 };
---
+++ src/DataImages.js
@@ -0,0 +1,66 @@
+import apu from './Assets/Images/Simpson/Apu.png';
+import bart from './Assets/Images/Simpson/bart.png';
+import burns from './Assets/Images/Simpson/burns.png';
+import carl from './Assets/Images/Simpson/Carl.png';
+import homer from './Assets/Images/Simpson/homer.png';
+import krusty from './Assets/Images/Simpson/krusty.png';
+import lenny from './Assets/Images/Simpson/Lenny.png';
+import lisa from './Assets/Images/Simpson/lisa.png';
+import maggie from './Assets/Images/Simpson/Maggie.png';
+import marge from './Assets/Images/Simpson/marge.png';
+import milhouse from './Assets/Images/Simpson/milhouse.png';
+import moe from './Assets/Images/Simpson/moe.png';
+import donut from './Assets/Images/Simpson/donut.png';
+
+export const backFace = donut;
+
+export const frontFace = [
+	{
+		img: apu,
+		name: 'Apu'
+	},
+	{
+		img: bart,
+		name: 'Bart'
+	},
+	{
+		img: burns,
+		name: 'Burns'
+	},
+	{
+		img: carl,
+		name: 'Carl'
+	},
+	{
+		img: homer,
+		name: 'Homer'
+	},
+	{
+		img: krusty,
+		name: 'Krusty'
+	},
+	{
+		img: lenny,
+		name: 'Lenny'
+	},
+	{
+		img: lisa,
+		name: 'Lisa'
+	},
+	{
+		img: maggie,
+		name: 'Maggie'
+	},
+	{
+		img: marge,
+		name: 'Marge'
+	},
+	{
+		img: milhouse,
+		name: 'Milhouse'
+	},
+	{
+		img: moe,
+		name: 'Moe'
+	}
+];
