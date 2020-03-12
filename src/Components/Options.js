b2a698420e45171c592699ad311e3315861e8fab
baphyv <baptiste.hyver@gmail.com>
2020-03-12 15:13:23 +0100

    added basic CSS in the homepage

--- src/Components/Options.js
+++ src/Components/Options.js
@@ -1,28 +1,40 @@
 import React from 'react';
 
+import '../CSS/options.css'
+
 import { Link } from 'react-router-dom';
+
+import Bounce from 'react-reveal'
 
 const Options = () => {
 	return (
-		<div>
-			<h1>
-				<u>Game options:</u>
-			</h1>
-			<select name="" id="">
-				<option value="">--Select a difficulty--</option>
-				<option value="easy">Easy</option>
-				<option value="medium">Medium</option>
-				<option value="hard">Hard</option>
-			</select>
-			<select name="" id="">
-				<option value="">--Select a theme--</option>
-				<option value="">Simpson</option>
-				<option value="">South Park</option>
-				<option value="">SpongeBob SquarePants</option>
-			</select>
-			<Link to='/game'>
-				<button>Start game</button>
-			</Link>
+		<div className="gameOptions">
+			<Bounce top>
+				<h1>
+					<u>Game options:</u>
+				</h1>
+			</Bounce>
+			<Bounce left>
+				<select name="" id="">
+					<option value="">--Select a difficulty--</option>
+					<option value="easy">Easy</option>
+					<option value="medium">Medium</option>
+					<option value="hard">Hard</option>
+				</select>
+			</Bounce>
+			<Bounce right>
+				<select name="" id="">
+					<option value="">--Select a theme--</option>
+					<option value="">Simpson</option>
+					<option value="">South Park</option>
+					<option value="">SpongeBob SquarePants</option>
+				</select>
+			</Bounce>
+			<Bounce bottom>
+				<Link to="/game">
+					<button>Start game</button>
+				</Link>
+			</Bounce>
 		</div>
 	);
 };
